e6864e7d36df48d2a830f6f0184a3cb798938c55
scratchy175 <yanntopilko@free.fr>
2021-11-28 03:03:58 +0100

    major fixes

--- sudoku.py
+++ sudoku.py
@@ -6,7 +6,6 @@ Elles représentent toutes des grilles de Sudoku valides à divers
 stades d'avancement: grille_0 est vide, grille_1 semi-remplie et
 grille_2 entièrement remplie.
 """
-from time import time
 from random import randint, shuffle
 
 
@@ -48,18 +47,18 @@ grille_2 = [
 ]
 
 grille_3 = [
-    [6, 2, 5, 8, 4, 3, 7, 9, 0],
+    [6, 2, 5, 8, 0, 3, 7, 9, 0],
     [7, 9, 0, 0, 0, 5, 4, 8, 0],
     [4, 8, 3, 9, 7, 1, 6, 2, 0],
     [8, 1, 4, 5, 9, 7, 2, 3, 0],
-    [2, 3, 6, 1, 8, 4, 9, 5, 0],
+    [2, 3, 6, 0, 0, 4, 9, 5, 0],
     [0, 5, 7, 3, 2, 6, 8, 1, 0],
     [0, 0, 0, 4, 3, 2, 1, 7, 0],
     [0, 0, 0, 7, 1, 8, 5, 6, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 grille_4 = [
-    [0, 2, 5, 8, 4, 3, 7, 9, 1],
+    [6, 2, 5, 8, 4, 3, 7, 9, 1],
     [7, 9, 1, 2, 6, 5, 4, 8, 3],
     [4, 8, 3, 9, 7, 1, 6, 2, 5],
     [8, 1, 4, 5, 9, 7, 2, 3, 6],
@@ -125,80 +124,70 @@ def region(x, i):
     return reg
 
 
-
-
-
 def ajouter(x, i, j, v):
     tmp = x[i-1][j-1]
     x[i-1][j-1] = v
     k = 3 * ((i - 1)//3) + ((j - 1)//3) + 1
     if unique(ligne(x, i)) and unique(colonne(x, j)) and unique(region(x, k)):
-        print("valeur correcte")
+        print("Valeur correcte !")
     else:
         x[i-1][j-1] = tmp
-        print("l'ajout est interdit")
+        print("Valeur incorrecte !")
     
 
 def verifier(x):
     for i in range(1, len(x)+1):
         for j in range(1, len(x)+1):
             k = 3 * ((i - 1)//3) + ((j - 1)//3) + 1
-            if not unique(ligne(x, i)) and not unique(colonne(x, j)) and not unique(region(x, k)):
-                return False          
+            if x[i-1][j-1] == 0:
+                print("Grille non valide !")
+                return False
+            elif not unique(ligne(x, i)) and not unique(colonne(x, j)) and not unique(region(x, k)):
+                print("Grille non valide !")
+                return False
+    print("Grille valide !")
     return True
-
+                
 
 def jouer(x):
-    while verifier(x):
-        i = int(input("Veuillez rentrer la ligne :"))
-        j = int(input("veuillez rentrer la colonne:"))
-        v = int(input("Veuillez rentrer la valeur a inserer:"))
+    while not verifier(x): 
+        i = int(input("Veuillez rentrer la ligne : "))
+        j = int(input("Veuillez rentrer la colonne : "))
+        v = int(input("Veuillez rentrer la valeur à insérer : "))
         ajouter(x, i, j, v)
         afficher(x)
+    print("Bravo ! Vous avez résolu la grille de sudoku !")
 
 
 def solution(x):
     dico = {d: [] for d in range(10)}
-
     for i in range(1,len(x)+1):
         for j in range(1,len(x)+1):
             k = 3 * ((i - 1)//3) + ((j - 1)//3) + 1
-            if x[i-1][j-1] == 0:     # changer ca
+            if x[i-1][j-1] == 0:
                 tmp = []
-                for y in range(1, 10):  # changer et pas mettre le 0
-
+                for y in range(1, 10):
                     if y not in ligne(x, i) and y not in colonne(x, j) and y not in region(x, k):
-                        tmp.append(y)
-                         
+                        tmp.append(y)    
                 dico[len(tmp)].append((i, j, tmp)) 
-            
     return dico
-
-# il faut in in ou not in et exclure les valeurs deja presentes dans la ligne colonne region puis ajouter au dico sous forme de tuples 
-#il fautr verif que le ligne colonne region n'est pas pleine avec la fonction unique 
-# Si les trois unique retournent true alors la valeur il faut la mettre dans une list ou quelque part
-
-# ou alors if x[i][j] in ligne colonne et region alors la tej de la list
-
-
-#rajouter des comprehension de liste
 
 
 def resoudre(x):
-    l = [list(solution((x)).values())[i][j] for i in range(len(solution(x))) for j in range(len(solution(x)[i]))]
-    if solution(x)[0]:
+    sol = solution(x)
+    l = [list(sol.values())[i][j] for i in range(len(sol)) for j in range(len(sol[i]))]   
+    if sol[0]:
         return False
     if not l:
         return x
     for i in range(len(l)):
         for j in range(len(l[i][2])):
-            x[l[i][0]-1][l[i][1]-1] = l[j][2][i]
+            x[l[i][0]-1][l[i][1]-1] = l[i][2][j]
             if resoudre(x):
-                return True
+                return x
             else:
                 x[l[i][0]-1][l[i][1]-1] = 0
         return False
-
 
 
 def generer(x):
@@ -218,19 +207,24 @@ def generer(x):
                 x[l[i][0]-1][l[i][1]-1] = 0
         return False 
 
-# si case vide ?????? ya pas il faut regarder en fonction des tuple non?
-# genre la premiere valeur a rentrer et la premiere valeur du tuple 
 
-grille_x = grille_0 # choisir la grille
+# Fonction avec nombre de case aléatoire à enlever >17
+def nouvelle(x):
+    k = 0
+    generer(x)
+    while k < randint(45,64):
+        i = randint(0,8)
+        j = randint(0,8)
+        if x[i][j] != 0:
+            x[i][j] = 0
+            k += 1  
+    print("Nouvelle grille généré avec succès !")   
+    return x
+    
 
-start_time = time()
-# resoudre(grille_x)
-# generer(grille_x)
-# afficher(grille_x)
-print(verifier(grille_x))
-print("La grille à été généré en {} secondes".format('%.3f'%(time() - start_time)))
-
-def nouvelle(x, lvl):
+# Petite variante de la fonction précedente avec 3 niveaux de difficulté
+"""
+def nouvelle(x, lvl):  
     val = 0
     k = 0
     if lvl == 1:
@@ -247,8 +241,18 @@ def nouvelle(x, lvl):
             x[i][j] = 0
             k += 1     
     return x
+"""
 
 
-(nouvelle(grille_x, 1))
-print("La grille à été généré en {} secondes".format('%.3f'%(time() - start_time)))
-ajouter(grille_1, 1, 1, 7)
+# choix grille à tester
+grille_x = grille_3 
+
+## Appel fonction de test
+
+# print(verifier(grille_x))
+# jouer(grille_4)
+# resoudre(grille_x)
+# generer(grille_x)
+# nouvelle(grille_x)
+# afficher(grille_x)
+
